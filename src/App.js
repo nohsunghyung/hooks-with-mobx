8d4305e1b9f816b188b6f5322d367602e8c11b0b
nohsunghyung <noh8085@scglab.com>
2020-10-23 09:49:27 +0900

    5555

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Notice from "./pages/Notice";
 
 function App() {
-  return <div className="App">44444-2222</div>;
+  return <div className="App">2222-2222</div>;
 }
 
 export default App;
