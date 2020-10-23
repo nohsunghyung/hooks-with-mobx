1b2000b30572df2eeb30003adf9ee28b5c4bd496
nohsunghyung <noh8085@scglab.com>
2020-10-23 09:31:36 +0900

    ㅅㅅㅅ

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function App() {
-<<<<<<< HEAD
-  return <div className="App">3333</div>;
-=======
   return <div className="App">1111</div>;
->>>>>>> parent of 77dd09b... 222
 }
 
 export default App;
