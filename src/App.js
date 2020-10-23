a3bf94fa2c618faaf71216347a0b804b88065c83
nohsunghyung <noh8085@scglab.com>
2020-10-23 09:22:10 +0900

    마스터 원복

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
-import Notice from "./pages/Notice";
+import React from "react";
 
 function App() {
+<<<<<<< HEAD
   return <div className="App">테스트1-1111111</div>;
+=======
+  return <div className="App"></div>;
+>>>>>>> parent of 71d8e28... init
 }
 
 export default App;
