01ffd804107d2f23f727a3b48022b7a14d9d8d01
nohsunghyung <noh8085@scglab.com>
2020-10-23 09:55:36 +0900

    ㅎ롱로

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from "react";
 import Notice from "./pages/Notice";
 
 function App() {
-<<<<<<< HEAD
-  return <div className="App">2222-2222</div>;
-=======
   return <div className="App">33333-2222</div>;
->>>>>>> parent of ee0a35f... 4444
 }
 
 export default App;
