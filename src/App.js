af6888c49c3edc634ff7e344d9074cae5b17475a
nohsunghyung <noh8085@scglab.com>
2020-10-23 09:52:12 +0900

    Revert "4444"
    This reverts commit ee0a35f1bb6c9c2221fd296231160a92e7088cc6.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Notice from "./pages/Notice";
 
 function App() {
-  return <div className="App">44444-2222</div>;
+  return <div className="App">33333-2222</div>;
 }
 
 export default App;
